7cd9d4243615727c3375afb0a8437cdedd81964a
Brandon205 <brandonblack02@gmail.com>
2019-10-11 08:45:04 -0700

    Fixed the /dogs/:id route

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ const db = require('./models');
 const app = express()
 app.use(express.urlencoded({extended: false}));
 
+function sendError() {
+    console.log('EEEEERRRRRROOOOORRRRR');
+}
+
 app.get('/', function(req, res) {
     res.send("You've reached the home page!");
 });
@@ -13,6 +17,18 @@ app.get('/dogs', function(req, res) {
     .then(function(dogs) {
         res.json(dogs);
     })
+});
+
+app.get('/dogs/:id', function(req, res) {
+    db.dog.findByPk(parseInt(req.params.id))
+    .then(function(foundDog) {
+        if (!foundDog) {
+            res.json([{'message': 'no dog found'}]);
+        } else {
+            res.json(foundDog);
+        }
+    })
+    .catch(sendError);
 });
 
 app.post('/dogs', function(req, res) {
@@ -37,7 +53,7 @@ app.put('/dogs/:id', function(req, res) {
             id: parseInt(req.params.id)
         }
     }).then(function(updatedDog) {
-        res.redirect('/dogs');
+        res.redirect(`/dogs/${req.params.id}`);
     })
 })
 
